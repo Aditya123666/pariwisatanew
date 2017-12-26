464513093ce92fb0ee45e133796831d01b1b4e79
Aditya123666 <Adityanugrahaadha9@gmail.com>
2017-12-26 23:14:04 +0700

    revisi project

---
+++ config/connect.php
@@ -0,0 +1,15 @@
+<?php
+	$host = "localhost";
+	$user = "root";
+	$pass = "123456";
+	
+	$db   = "db_pkl2225";
+	
+	$pdo = new PDO("mysql:host=$host; dbname=$db", $user, $pass);
+	//Hanya TES
+	/*if($pdo){
+		echo "Koneksi ke DB Berhasil";
+	}else{
+		echo "Koneksi GAGAL";
+	}*/
+?>
---
+++ config/koneksi.php
@@ -0,0 +1,28 @@
+<?php
+	$host	= "localhost";
+	$user	= "root";
+	$pass	= "";
+	$db		= "tourtravel";
+	
+	//$kon = mysqli_connect($host, $user, $pass);
+	//$kondb = mysqli_select_db ($db,$kon);
+	//edit
+	$kon = mysqli_connect($host, $user, $pass,$db);
+	//$kondb = mysqli_select_db ($db,$kon);
+	
+	
+	
+	//test koneksi
+	
+	/*if($kon){
+		echo "Terkoneksi dengan mysqliiiiii";
+		if($kondb){
+			echo "DataBase $db yang dipilih";
+		}else{
+			echo "DataBase tidak ada";
+		}
+	}else{
+		echo "Koneksi GAGAL";
+	}
+	*/
+?>
